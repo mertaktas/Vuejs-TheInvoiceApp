c4b5cf7b4f86606e84035aaadb949783bac319b3
Mert Aktaş <mertaktas231@gmail.com>
2021-07-09 17:59:38 +0300

    filtered invoices

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,12 +4,12 @@
     <div class="header flex">
       <div class="left flex flex-column">
         <h1>Invoices</h1>
-        <span>There are 4 total invoices</span>
+        <span>There are {{ invoiceData.length }} total invoices</span>
       </div>
       <div class="right flex">
         <div @click="toggleFilterMenu" class="filter flex">
           <span
-            >Filter by status</span
+            >Filter by status <span v-if="filteredInvoice">: {{ filteredInvoice }}</span></span
           >
           <img src="@/assets/icon-arrow-down.svg" alt="" />
           <ul v-show="filterMenu" class="filter-menu">
@@ -27,9 +27,9 @@
         </div>
       </div>
     </div>
-    <!-- Invoice Data -->
+    <!-- Invoices -->
     <div v-if="invoiceData.length > 0">
-      <Invoice v-for="(invoice, index) in invoiceData" :invoice="invoice" :key="index" />
+      <Invoice v-for="(invoice, index) in filteredData" :invoice="invoice" :key="index" />
     </div>
     <div v-else class="empty flex flex-column">
       <img src="@/assets/illustration-empty.svg" alt="" />
@@ -40,14 +40,15 @@
 </template>
 
 <script>
-import Invoice from '@/components/Invoice.vue'
-import { mapMutations, mapState } from 'vuex'
+import Invoice from "../components/Invoice";
+import { mapMutations, mapState } from "vuex";
 export default {
   name: "Home",
-  data () {
+  data() {
     return {
       filterMenu: null,
-    }
+      filteredInvoice: null,
+    };
   },
   components: {
     Invoice,
@@ -57,13 +58,34 @@ export default {
     newInvoice() {
       this.TOGGLE_INVOICE();
     },
-    toggleFilterMenu () {
+    toggleFilterMenu() {
       this.filterMenu = !this.filterMenu;
-    }
+    },
+    filteredInvoices(e) {
+      if (e.target.innerText === "Clear Filter") {
+        this.filteredInvoice = null;
+        return;
+      }
+      this.filteredInvoice = e.target.innerText;
+    },
   },
   computed: {
     ...mapState(["invoiceData"]),
-  }
+    filteredData() {
+      return this.invoiceData.filter((invoice) => {
+        if (this.filteredInvoice === "Draft") {
+          return invoice.invoiceDraft === true;
+        }
+        if (this.filteredInvoice === "Pending") {
+          return invoice.invoicePending === true;
+        }
+        if (this.filteredInvoice === "Paid") {
+          return invoice.invoicePaid === true;
+        }
+        return invoice;
+      });
+    },
+  },
 };
 </script>
 
